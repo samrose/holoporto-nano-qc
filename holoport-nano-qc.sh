b28ec884056bcb4ed6dd82d3415974c36ed2e274
samrose <samuel.rose@gmail.com>
2019-10-09 17:36:30 -0400

    Update holoport-nano-qc.sh

--- holoport-nano-qc.sh
+++ holoport-nano-qc.sh
@@ -1,4 +1,4 @@
 #!@bash@/bin/bash
 
 
-fulload() { yes > /dev/null | yes > /dev/null | yes > /dev/null | yes > /dev/null  & }; fulload; read; 
+timeout 30s bash -c 'fulload() { yes > /dev/null | yes > /dev/null | yes > /dev/null | yes > /dev/null  & }; fulload; read;killall yes' 
