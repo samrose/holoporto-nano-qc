c56e126734fcdc94dd2bc738c484715632aae787
samrose <samuel.rose@gmail.com>
2019-10-10 11:09:43 -0400

    holoport-nano-qc:30s timed cpu load

--- holoport-nano-qc.sh
+++ holoport-nano-qc.sh
@@ -1,5 +1,31 @@
 #!@bash@/bin/bash
 
+timeout 30s bash -c '
+function check1() {
+  return 1
+}
 
-timeout 30s bash -c 'fulload() { yes > /dev/null | yes > /dev/null | yes > /dev/null | yes > /dev/null  & }; fulload; read; killall yes'
-timeout 30s bash -c ' cat /sys/class/thermal/thermal_zone*/temp'
+function full_load() {
+  yes > /dev/null |
+  yes > /dev/null |
+  yes > /dev/null |
+  yes > /dev/null
+}
+function get_temp(){
+  for temp in /sys/class/thermal/thermal_zone*/temp; do
+    tmpstr=$(<$temp)
+    cputmp=$((tmpstr))
+    hightmp=$((99000))
+    if [ $cputmp -gt $hightmp ]
+      then
+        echo $cputmp
+    fi
+  done
+}
+full_load &
+while [ check1 ]
+do
+  get_temp;
+done
+killall yes
+'
